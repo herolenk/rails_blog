c655a4786c64310579c894f152cd9dd2e9a8012b
herolenk <herolenk@hotmail.com>
2017-07-13 07:19:51 +0300

    Updating Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,10 @@ class ArticlesController < ApplicationController
 def new
   @article = Article.new
 end
- 
+
+def edit
+	@article = Article.find(params[:id])
+end
 def create
   @article = Article.new(article_params)
  
@@ -23,7 +26,17 @@ def create
     render 'new'
   end
 end
- 
+
+def update
+	@article = Article.find(params[:id])
+
+	if @article.update(article_params)
+		redirect_to @article
+
+	else
+		render 'edit'
+	end
+end 
 private
   def article_params
     params.require(:article).permit(:title, :text)
